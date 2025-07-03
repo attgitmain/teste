108dbb7f926d2c21bb35ec5f5f9ed02cdff4f501
attgitmain <skyteambr2020@gmail.com>
2025-07-03 17:57:24 -0300

    feat(tv): enlarge fonts for painel tv (#140)

--- frontend/src/components/PainelTV/GroupedTickets.tsx
+++ frontend/src/components/PainelTV/GroupedTickets.tsx
@@ -13,15 +13,15 @@ const backendUrl = getBackendUrl();
 
 const GroupedTickets: React.FC<GroupedTicketsProps> = ({ user, tickets }) => {
   return (
-    <Paper variant="outlined" sx={{ p: 1 }} component={motion.div} layout>
+    <Paper variant="outlined" sx={{ p: 2 }} component={motion.div} layout>
       <Box display="flex" alignItems="center" mb={1}>
         <Avatar
           src={user.profileImage ? `${backendUrl}/public/company${user.companyId}/user/${user.profileImage}` : undefined}
-          sx={{ mr: 1 }}
+          sx={{ mr: 1, width: 56, height: 56 }}
         />
         <Box>
-          <Typography variant="subtitle1">{user.name}</Typography>
-          <Typography variant="caption">Atendimentos: {tickets.length}</Typography>
+          <Typography variant="h6">{user.name}</Typography>
+          <Typography variant="subtitle1">Atendimentos: {tickets.length}</Typography>
         </Box>
       </Box>
       <AnimatePresence>{tickets.map((t) => <TicketCard key={t.id} ticket={t} />)}</AnimatePresence>
