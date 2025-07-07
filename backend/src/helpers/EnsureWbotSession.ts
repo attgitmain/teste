5c8c8ef5e7b71fdde3e0719cbeeb84e4b367c53c
attgitmain <skyteambr2020@gmail.com>
2025-07-07 14:31:50 -0300

    fix: accept wbot session

--- backend/src/helpers/EnsureWbotSession.ts
+++ backend/src/helpers/EnsureWbotSession.ts
@@ -1,11 +1,15 @@
 import { getWbot } from "../libs/wbot";
 import Whatsapp from "../models/Whatsapp";
 import AppError from "../errors/AppError";
+import { WASocket } from "@whiskeysockets/baileys";
 
-const EnsureWbotSession = (whatsapp: Whatsapp) => {
-  const wbot = getWbot(whatsapp.id);
+type Session = WASocket & {
+  id?: number;
+};
+const EnsureWbotSession = (input: Whatsapp | Session): Session => {
+  const wbot = ("ws" in input ? (input as Session) : getWbot(input.id));
 
-  if (!wbot || !wbot.ws || wbot.ws.readyState !== "open" || !wbot.user) {
+  if (!wbot || !wbot.ws || (wbot.ws as any).readyState !== "open" || !wbot.user) {
     throw new AppError("ERR_WAPP_SESSION_NOT_READY");
   }
 
