0bbc308cb6c94d09adb3b6335391d4f66dfbf9f3
Bruno Nogueira <skyteambr2020@gmail.com>
2025-07-11 19:25:00 -0300

    refactor: use env vars for efi config

--- backend/src/config/Gn.ts
+++ backend/src/config/Gn.ts
@@ -1,46 +1,14 @@
-/* import path from "path";
-
- const name = process.env.GERENCIANET_SANDBOX === "false" ? "producao" : "homologacao";
-
- const cert = path.join(
-  __dirname,
-  `../../certs/${process.env.GERENCIANET_PIX_CERT}.p12`
-);
-
-export = {
-  sandbox: process.env.GERENCIANET_SANDBOX === "true",
-  client_id: process.env.GERENCIANET_CLIENT_ID as string,
-  client_secret: process.env.GERENCIANET_CLIENT_SECRET as string,
-  pix_cert: cert
-};
-*/
-
 import path from "path";
-import Setting from "../models/Setting";
 
-const cert = path.join(__dirname, "../../certs/certificadoEfi.p12");
-
-const getSettingValue = async (key: string): Promise<string | undefined> => {
-  try {
-    const buscacompanyId = 1;
-    const setting = await Setting.findOne({
-      where: { companyId: buscacompanyId, key }
-    });
-    return setting?.value;
-  } catch (error) {
-    console.error("Error retrieving setting:", error);
-    return undefined;
-  }
-};
-
-const getGnConfig = async () => {
-  const client_id = (await getSettingValue("eficlientid")) as string;
-  const client_secret = (await getSettingValue("eficlientsecret")) as string;
+const getGnConfig = () => {
+  const cert = process.env.GERENCIANET_PIX_CERT
+    ? path.resolve(process.env.GERENCIANET_PIX_CERT)
+    : "";
 
   return {
-    sandbox: false,
-    client_id,
-    client_secret,
+    sandbox: process.env.GERENCIANET_SANDBOX === "true",
+    client_id: process.env.GERENCIANET_CLIENT_ID as string,
+    client_secret: process.env.GERENCIANET_CLIENT_SECRET as string,
     pix_cert: cert
   };
 };
