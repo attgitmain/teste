ec595b769c2ad8e1a25d577b871306bc35c5085b
Bruno Nogueira <skyteambr2020@gmail.com>
2025-07-13 16:41:58 -0300

    restart WhatsApp session if disconnected

--- backend/src/helpers/isWhatsappConnected.ts
+++ backend/src/helpers/isWhatsappConnected.ts
@@ -1,13 +1,22 @@
 import Whatsapp from "../models/Whatsapp";
 import GetWhatsappWbot from "./GetWhatsappWbot";
 import EnsureWbotSession from "./EnsureWbotSession";
+import { StartWhatsAppSession } from "../services/WbotServices/StartWhatsAppSession";
 
 const isWhatsappConnected = async (whatsapp: Whatsapp): Promise<boolean> => {
   try {
+    // try to ensure current session is active
     EnsureWbotSession(await GetWhatsappWbot(whatsapp));
     return true;
   } catch {
-    return false;
+    // attempt to restart the session once
+    try {
+      await StartWhatsAppSession(whatsapp, whatsapp.companyId);
+      EnsureWbotSession(await GetWhatsappWbot(whatsapp));
+      return true;
+    } catch {
+      return false;
+    }
   }
 };
 
