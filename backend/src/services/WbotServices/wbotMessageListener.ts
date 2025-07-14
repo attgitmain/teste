c1165f815b6c79ee50f1de0426e9363d6fe0eaeb
Bruno Nogueira <skyteambr2020@gmail.com>
2025-07-13 23:42:14 -0300

    Remove userId check from verifyRating

--- backend/src/services/WbotServices/wbotMessageListener.ts
+++ backend/src/services/WbotServices/wbotMessageListener.ts
@@ -3308,7 +3308,6 @@ export const verifyRating = (ticketTraking: TicketTraking) => {
     ticketTraking &&
     ticketTraking.finishedAt === null &&
     ticketTraking.closedAt !== null &&
-    ticketTraking.userId !== null &&
     ticketTraking.ratingAt === null
   ) {
     return true;
