eedb2c9efa2ec38422aca281d1d1b5dfd77a330c
Bruno Nogueira <skyteambr2020@gmail.com>
2025-07-13 23:03:31 -0300

    docs: add pm2 env details (#211)

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -3,6 +3,11 @@ module.exports = [{
   name: 'multipremium-back',
   exec_mode: 'fork',
   cron_restart: '05 00 * * *',
+  cwd: __dirname,
+  env_file: '.env',
+  env: {
+    NODE_ENV: 'production'
+  },
   max_memory_restart: '769M', // Configuração para reiniciar quando atingir 769 MB de memória
   node_args: '--max-old-space-size=769', // Limite de memória do Node.js para 769 MB
   watch: false
